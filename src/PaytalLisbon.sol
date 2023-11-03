c5741d2fe04d01369a0a7a0777a649cf55b5fbfe
josemariasosa <jose.skedar@gmail.com>
2023-11-03 20:18:26 +0000

    open zeppelin

--- src/PaytalLisbon.sol
+++ src/PaytalLisbon.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "chronicle-std/IChronicle.sol";
+import "chronicle-std/src/IChronicle.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
 
 enum Currency {
     USDT,
@@ -16,11 +19,25 @@ struct Coin {
 
 contract PaytalLisbon {
 
+    uint constant public TOTAL_CURRENCIES = 3;
+
+    mapping (Currency => address) tokenAddress;
+
     function getPriceCombination(
         uint256 price,
         address account
     ) public returns(Coin[] memory) {
         // Tokens de usuario
+
+        uint256[] memory userTokens;
+
+        for (uint i=0; i < TOTAL_CURRENCIES; ++i) {
+            userTokens.push(
+                IERC20(tokenAddress[Currency(i)])
+            );
+        }
+
+
         // Transformarlo a USD
         // Armar la respuesta.
     }
