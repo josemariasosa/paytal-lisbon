20a3f3296dafd2039f1c9d7e24bf80a50a9e480a
josemariasosa <jose.skedar@gmail.com>
2023-11-03 22:34:56 +0000

    ETH Lisbon 🐟🐠

--- src/PaytalLisbon.sol
+++ src/PaytalLisbon.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import "chronicle-std/src/IChronicle.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
+import "forge-std/console.sol";
 
 enum Currency {
     USDT,
@@ -19,38 +19,91 @@ struct Coin {
 
 contract PaytalLisbon {
 
+    uint256[3] TEST_DATA = [999_998, 1 ether, 1 ether];
+    
     uint constant public TOTAL_CURRENCIES = 3;
 
     mapping (Currency => address) tokenAddress;
+    mapping (Currency => address) chronicleOracleAddress;
+
+    constructor (
+        address usdt,
+        address eth,
+        address gno,
+        address usdtUsd,
+        address ethUsd,
+        address gnoUsd
+    ) {
+        console.log(usdt);
+        console.log(eth);
+        console.log(gno);
+        console.log(usdtUsd);
+        console.log(ethUsd);
+        console.log(gnoUsd);
+
+        tokenAddress[Currency.USDT] = usdt;
+        tokenAddress[Currency.ETH] = eth;
+        tokenAddress[Currency.GNO] = gno;
+        chronicleOracleAddress[Currency.USDT] = usdtUsd;
+        chronicleOracleAddress[Currency.ETH] = ethUsd;
+        chronicleOracleAddress[Currency.GNO] = gnoUsd;
+    }
 
     function getPriceCombination(
         uint256 price,
         address account
-    ) public returns(Coin[] memory) {
+    ) public view returns(uint256[3] memory, bool success) {
         // Tokens de usuario
 
-        uint256[] memory userTokens;
+        uint256[3] memory result;
+        uint256 _tokenBalance;
+        uint256 _totalAmount;
+        uint256 _accum;
+        uint256 _unit;
 
         for (uint i=0; i < TOTAL_CURRENCIES; ++i) {
-            userTokens.push(
-                IERC20(tokenAddress[Currency(i)])
-            );
+            Currency _currency = Currency(i);
+            _tokenBalance = IERC20(tokenAddress[_currency]).balanceOf(account);
+            // _decimals = IERC20(tokenAddress[_currency]).decimals();
+            uint256 priceInUsd = IChronicle(chronicleOracleAddress[_currency]).read();
+            // uint256 priceInUsd = TEST_DATA[i];
+
+            console.log("ESTAMOS ACAA");
+            console.log(i);
+
+            // TODO: Patching decimals for USDT
+            if (i == 0) {
+                _unit = 1_000_000;
+            } else {
+                _unit = 1e18;
+            }
+
+            console.log("priceInUsd Y _tokenBalance _unit");
+            console.log(priceInUsd);
+            console.log(_tokenBalance);
+            console.log(_unit);
+
+            _totalAmount = priceInUsd * _tokenBalance / _unit;
+            _accum += _totalAmount;
+
+            console.log("ACCUM Y TOTAL AMOUNT");
+            console.log(_accum);
+            console.log(_totalAmount);
+
+            if (_accum >= price) {
+                result[i] = _totalAmount - (_accum - price);
+                console.log("por aquiA");
+                break;
+            } else {
+                result[i] = _totalAmount;
+                console.log("por aca");
+            }
         }
+        console.log("RESULTS:");
+        console.log(result[0]);
+        console.log(result[1]);
+        console.log(result[2]);
 
-
-        // Transformarlo a USD
-        // Armar la respuesta.
-    }
-
-
-
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
+        return (result, _accum >= price);
     }
 }
--- test/PaytalLisbon.t.sol
+++ test/PaytalLisbon.t.sol
@@ -3,22 +3,63 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {PaytalLisbon} from "../src/PaytalLisbon.sol";
+import {Erc20Token} from "../src/Erc20Token.sol";
+import {Chronicles} from "../src/Chronicles.sol";
 
-contract CounterTest is Test {
-    PaytalLisbon public counter;
+import "forge-std/console.sol";
+
+
+contract PaytalTest is Test {
+    address[] accounts = [address(100_000), address(100_001), address(100_002)];
+
+    PaytalLisbon public paytal;
+    Erc20Token public usdt;
+    Erc20Token public eth;
+    Erc20Token public gno;
+
+    Chronicles public usdtUsd;
+    Chronicles public ethUsd;
+    Chronicles public gnoUsd;
 
     function setUp() public {
-        counter = new PaytalLisbon();
-        counter.setNumber(0);
+        usdt = new Erc20Token();
+        eth = new Erc20Token();
+        gno = new Erc20Token();
+
+        usdtUsd = new Chronicles(999_998);
+        ethUsd = new Chronicles(1_820 ether);
+        gnoUsd = new Chronicles(112 ether);
+
+        paytal = new PaytalLisbon(
+            address(usdt),
+            address(eth),
+            address(gno),
+            address(usdtUsd),
+            address(ethUsd),
+            address(gnoUsd)
+        );
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_pay_with_first_token() public {
+        usdt.mint(accounts[0], 100 * 1e6);
+        paytal.getPriceCombination(90 * 1e6, accounts[0]);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_pay_with_second_token() public {
+        usdt.mint(accounts[0], 30 * 1e6);
+        eth.mint(accounts[0], 0.044 * 1e18);
+        paytal.getPriceCombination(90 * 1e6, accounts[0]);
     }
+
+    function test_pay_with_third_token() public {
+        usdt.mint(accounts[0], 30 * 1e6);
+        eth.mint(accounts[0], 0.0003 * 1e18);
+        gno.mint(accounts[0], 100 * 1e18);
+        paytal.getPriceCombination(90 * 1e6, accounts[0]);
+    }
+
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
 }
